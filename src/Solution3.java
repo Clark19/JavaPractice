e7913b10f047d24a632c6e70cd79c645c3a6d77b
Clark19 <clarkgable1@gmail.com>
2020-11-08 16:43:30 +0900

    feat:  '동전뒤집기게임(마틴게일 베팅법)' 기능 구현 1차 완료

--- src/Solution3.java
+++ src/Solution3.java
@@ -1,5 +1,7 @@
 public class Solution3 {
 
+    private final int BASIC_STAKE = 100;
+
     public int solution(int money, String[] expected, String[] actual) {
         int answer = -1;
 
@@ -16,17 +18,25 @@ public class Solution3 {
         }
 
         //1.
-        int m = 100; //  판돈
-        if (expected[0].equals(actual[0])) {
-            //win : get money
-            answer += m;
-        } else {
-            //loose monney
-            answer -= m;
-            m *= 2;
-        }
+        int stake = BASIC_STAKE; //  판돈
+        for (int i=0; money > 0 && i < actual.length; i++) {
+
+            if (expected[i].equals(actual[i])) { //win : get money
+                money += stake;
+                if (money >= BASIC_STAKE)
+                    stake = BASIC_STAKE;
+                else
+                    stake = money;
+            } else { //lose: lose monney
+                money -= stake;
+                if (money >= stake * 2)
+                    stake *= 2;
+                else
+                    stake = money;
+            }
 //        100
-//        answer =
+        }
+        answer = money;
 
         //2.
         return answer;
@@ -41,18 +51,30 @@ money	expected	                        actual	                                re
 1500	['H', 'H', 'H', 'T', 'H']	         ['T', 'T', 'T', 'H', 'T']	            0
  */
         //테스트 값
+// 테스트값       개선 할 점 생각해볼것
         final int money1 = 1000;
         final String[] expected1 = {"H", "T", "H", "T", "H", "T", "H"};
         final String[] actual1 = {"T", "T", "H", "H", "T", "T", "H"};
         final int expectedResult1 = 1400;
 
+        final int money2 = 1200;
+        final String[] expected2 = {"T", "T", "H", "H", "H"};
+        final String[] actual2 = {"H", "H", "T", "H", "T"};
+        final int expectedResult2 = 900;
+
+        final int money3 = 1500;
+        final String[] expected3 = {"H", "H", "H", "T", "H"};
+        final String[] actual3 = {"T", "T", "T", "H", "T"};
+        final int expectedResult3 = 0;
+
+        //테스트 값 변경하면서 대입하는거 개선할 방법 생각해 볼 것.
         final int money = money1;
         final String[] expected = expected1;
         final String[] actual = actual1;
         final int expectedResult = expectedResult1;
 
 
-        prt("우아한테크코스-프로그래머스 코딩테스트 1번 문제 : '동전뒤집기게임(마틴게일 베팅법)'");
+        prt("우아한테크코스-프로그래머스 코딩테스트 3번 문제 : '동전뒤집기게임(마틴게일 베팅법)'");
         int actualResult = solution(money, expected, actual);
         if ( expectedResult == actualResult)
             prt("Test 성공");
